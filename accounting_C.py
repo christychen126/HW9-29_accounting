0d78f5e487059922a028d7c6050af2f27cfe46ff
Christy Chen <christychen126@gmail.com>
2016-09-29 21:12:33 -0700

    Add Docstring

--- accounting_C.py
+++ accounting_C.py
@@ -5,7 +5,14 @@ melon_count_tracking = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
 
 
 def revenue_of_melons(melon_order_report, melon_prices, melon_count_tracking):
-    
+    """ Calculate the revenue of each type of melons
+
+    Open report for melon orders. Update the counts for each type of melons by going
+    throuhg all orders. Calculate the revenue of each type of melons by multiply 
+    price and count. Print out the summerized revenue statements for each type 
+    of melons.      
+    """
+
     report = open(melon_order_report)
     
     for line in report:
@@ -18,6 +25,7 @@ def revenue_of_melons(melon_order_report, melon_prices, melon_count_tracking):
 
     report.close()
 
+#call the function
 revenue_of_melons("orders-by-type.txt", melon_prices, melon_count_tracking)
   
     
@@ -25,7 +33,12 @@ print "*" * 42
 
 
 def sales_type_revenue(sales_type_report):
-    
+    """Compare revenues of sales team and online sales
+
+    Open sales report. Calculate the revenu of each type of sales by adding up 
+    the revenue of each order accordingly. Print summerized statements for both type of 
+    sales. Compare the revenue and print out the corresponding statement. 
+    """    
     report = open(sales_type_report)
 
     sales_people = 0
@@ -46,6 +59,8 @@ def sales_type_revenue(sales_type_report):
     else:
         print "Time to fire the sales team! Online sales rule all!"
 
+
+#call the function
 sales_type_revenue("orders-with-sales.txt")
 
 print "*" * 42
